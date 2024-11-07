490038e63d482b215da6560627581f75f56828e4
chrox <chrox.huang@gmail.com>
2024-11-07 14:51:45 +0100

    Disable PostHog in development

--- apps/readest-app/src/context/PHContext.tsx
+++ apps/readest-app/src/context/PHContext.tsx
@@ -3,7 +3,11 @@ import { ReactNode } from 'react';
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 
-if (typeof window !== 'undefined' && process.env['NEXT_PUBLIC_POSTHOG_KEY']) {
+if (
+  typeof window !== 'undefined' &&
+  process.env['NODE_ENV'] === 'production' &&
+  process.env['NEXT_PUBLIC_POSTHOG_KEY']
+) {
   posthog.init(process.env['NEXT_PUBLIC_POSTHOG_KEY'], {
     api_host: process.env['NEXT_PUBLIC_POSTHOG_HOST'],
     person_profiles: 'always',
