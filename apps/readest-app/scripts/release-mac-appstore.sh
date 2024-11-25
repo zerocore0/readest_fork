a67bfa3591d421fb897e32e355f08fb432e5049c
chrox <chrox.huang@gmail.com>
2024-11-25 10:12:47 +0100

    Build conf for app store

---
+++ apps/readest-app/scripts/release-mac-appstore.sh
@@ -0,0 +1,8 @@
+pnpm run build-macos-universial-appstore
+
+BUNDLE_DIR=src-tauri/target/universal-apple-darwin/release/bundle/macos
+APP_BUNDLE=$BUNDLE_DIR/Readest.app
+INSTALLER_BUNDLE=$BUNDLE_DIR/Readest.pkg
+
+xcrun productbuild --sign "$APPLE_INSTALLER_SIGNING_IDENTITY" --component $APP_BUNDLE /Applications $INSTALLER_BUNDLE
+xcrun altool --upload-app --type macos --file $INSTALLER_BUNDLE --apiKey $APPLE_API_KEY --apiIssuer $APPLE_API_ISSUER
